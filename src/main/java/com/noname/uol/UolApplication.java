e85b1e2f4c35373f7b14fcf134e65d385ecf645a
ZeroPirata <gabriel0gsbn@gmail.com>
2022-08-31 14:27:49 -0300

    Inicio backend

---
+++ src/main/java/com/noname/uol/UolApplication.java
@@ -0,0 +1,13 @@
+package com.noname.uol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noname/uol/UolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noname.uol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
