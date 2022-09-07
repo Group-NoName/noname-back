d351016457755d450033abfcf6ae882c052fb1e0
ZeroPirata <gabriel0gsbn@gmail.com>
2022-09-07 17:13:09 -0300

    feature - tratamento de erros iniciais criado

---
+++ src/main/java/com/noname/uol/recursos/ExecaoDeRecurso.java
@@ -0,0 +1,21 @@
+package com.noname.uol.recursos;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.noname.uol.servicos.execao.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ExecaoDeRecurso {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<TratamentoDeErro> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+	HttpStatus status = HttpStatus.NOT_FOUND;
+	TratamentoDeErro err = new TratamentoDeErro(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+	}
+}
