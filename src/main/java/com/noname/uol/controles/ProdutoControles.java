a9f1cdd96ceb215d4ec10a422839dc1dcd8a043e
lucasdwn <lfcosta0804@gmail.com>
2022-09-20 11:52:51 -0300

    adicionando limitacao de produtos semelhantes no map

--- src/main/java/com/noname/uol/controles/ProdutoControles.java
+++ src/main/java/com/noname/uol/controles/ProdutoControles.java
@@ -51,8 +51,8 @@ public class ProdutoControles {
 		return  ResponseEntity.ok().body(new produtoDTO(produto));
 	}
 	
-	@GetMapping("/produtos-semelhantes/{id}")
-	public ResponseEntity<List<produtoDTO>> ObterProdutoSemelhante(@PathVariable String id){
+	@GetMapping("/produtos-semelhantes/{id}/{quantia}")
+	public ResponseEntity<List<produtoDTO>> ObterProdutoSemelhante(@PathVariable String id, @PathVariable String quantia){
 		
 		List<Tags> tagsProdutoAlvo = produtoServico.findById(id).getTags();
 		List<Produtos> todosOsProdutos = produtoServico.findAll()				;
@@ -78,6 +78,7 @@ public class ProdutoControles {
 		List<produtoDTO> produtoDto = produtosSortidos
 				.stream()
 				.map(x -> new produtoDTO(x))
+				.limit(Integer.parseInt(quantia))
 				.collect(Collectors.toList());		
 		
 		return ResponseEntity.ok().body(produtoDto);
