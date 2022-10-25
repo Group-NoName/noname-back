832e4e98b71c102e0d7e05a2fe0f496c70234ab3
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-25 20:34:31 -0300

    Fix - Correção do inicializador

--- src/main/java/com/noname/uol/UolApplication.java
+++ src/main/java/com/noname/uol/UolApplication.java
@@ -2,10 +2,14 @@ package com.noname.uol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableSwagger2
+@SpringBootApplication
+@EnableFeignClients
 public class UolApplication {
 
 	public static void main(String[] args) {
