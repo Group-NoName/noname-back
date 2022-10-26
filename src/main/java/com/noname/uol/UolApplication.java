a491dafd41ea75c35bee2227131f37e68c496db2
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-26 16:04:18 -0300

    Fix - Connection port and run file

--- src/main/java/com/noname/uol/UolApplication.java
+++ src/main/java/com/noname/uol/UolApplication.java
@@ -2,14 +2,14 @@ package com.noname.uol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @EnableSwagger2
-@SpringBootApplication
-@EnableFeignClients
+@EnableMongoRepositories
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class UolApplication {
 
 	public static void main(String[] args) {
